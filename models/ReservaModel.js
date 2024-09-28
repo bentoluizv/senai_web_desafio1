7baa034db69876cc7ddff7cb1baaa7ae26bc4d82
bentoluizv <bentoluizv@gmail.com>
2024-09-28 00:22:10 -0300

    add Reserva Model w/ local storage logic

---
+++ models/ReservaModel.js
@@ -0,0 +1,47 @@
+export class Reserva {
+  constructor(placa, proprietario, apartamento, bloco, modelo, cor) {
+    this.placa = placa;
+    this.proprietario = proprietario;
+    this.apartamento = apartamento;
+    this.bloco = bloco;
+    this.modelo = modelo;
+    this.cor = cor;
+  }
+}
+
+export class ReservaModel {
+  reservas = [];
+
+  constructor() {
+    this.#syncWithStorage();
+  }
+
+  register(reserva) {
+    this.reservas.push(reserva);
+    this.#sendToStorage();
+  }
+
+  list() {
+    this.#syncWithStorage();
+    return this.reservas.reverse();
+  }
+
+  #sendToStorage() {
+    window.localStorage.setItem("reservas", JSON.stringify(this.reservas));
+  }
+
+  #syncWithStorage() {
+    const localData = window.localStorage.getItem("reservas");
+    if (!localData) {
+      return;
+    }
+
+    this.reservas = JSON.parse(localData);
+
+    if (!Array.isArray(this.reservas)) {
+      throw new Error("Revervas is not an Array");
+    }
+
+    this.reservas = this.reservas;
+  }
+}
