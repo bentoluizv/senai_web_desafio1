55fb722fcb21e14ce08ddd267f5bf68126e6b025
bentoluizv <bentoluizv@gmail.com>
2024-09-28 09:58:08 -0300

    improve listing items style

--- views/ReservaView.js
+++ views/ReservaView.js
@@ -5,11 +5,57 @@ export class ReservaView {
   listar() {
     const listagem = document.getElementById("listagem");
     const reservas = this.controller.listar();
+
     for (let index = 0; index < reservas.length; index++) {
       const reserva = reservas[index];
+
       const li = document.createElement("li");
-      li.classList.add("p-4", "border", "border-blue-400");
-      li.textContent = `${reserva.placa} - ${reserva.proprietario} - ${reserva.modelo} (${reserva.cor})`;
+      li.id = reserva.uuid;
+      li.classList.add(
+        "card",
+        "bg-gray-100",
+        "text-gray-800",
+        "p-4",
+        "rounded-lg",
+        "shadow-md",
+        "hover:bg-blue-500",
+        "hover:text-white",
+        "transition-all",
+        "duration-300"
+      );
+      const div = document.createElement("div");
+      div.classList.add("flex", "justify-between");
+
+      const h3 = document.createElement("h3");
+      h3.classList.add("font-bold", "text-lg");
+
+      const close = document.createElement("button");
+      close.classList.add(
+        "card-close-btn",
+        "font-bold",
+        "text-xl",
+        "text-center",
+        "cursor-pointer"
+      );
+
+      const p_1 = document.createElement("p");
+      const p_2 = document.createElement("p");
+      const p_3 = document.createElement("p");
+
+      h3.textContent = `Bloco: ${reserva.bloco} | Ap: ${reserva.apartamento}`;
+
+      close.textContent = "X";
+
+      p_1.textContent = `${reserva.proprietario}`;
+      p_2.textContent = `${reserva.placa}`;
+      p_3.textContent = `${reserva.modelo} (${reserva.cor})`;
+
+      div.appendChild(h3);
+      div.appendChild(close);
+      li.appendChild(div);
+      li.appendChild(p_1);
+      li.appendChild(p_2);
+      li.appendChild(p_3);
       listagem.appendChild(li);
     }
   }
