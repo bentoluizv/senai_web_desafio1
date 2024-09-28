5057351c0f1b38d30281c37a27f3d080e4e5dbca
bentoluizv <bentoluizv@gmail.com>
2024-09-28 10:49:55 -0300

    fix bug that page will only list data when acessed via "/index.html"

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ import loadSampleData from "./utils/loadSampleData";
 import { ReservaView } from "./views/ReservaView";
 
 await loadSampleData();
+
 const reservaModel = new ReservaModel();
 const reservaController = new ReservaController(reservaModel);
 const reservaView = new ReservaView(reservaController);
 
-if (window.location.pathname === "/index.html") {
+if (
+  window.location.pathname === "/index.html" ||
+  window.location.pathname === "/"
+) {
   reservaView.list();
   reservaView.delete();
 }
