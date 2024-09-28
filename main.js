fba74f845e845d509337115c8cff6fcb09adddf2
bentoluizv <bentoluizv@gmail.com>
2024-09-28 00:24:14 -0300

    add main.js to orchestrate page assembly

---
+++ main.js
@@ -0,0 +1,17 @@
+import { ReservaController } from "./controllers/ReservaController";
+import { ReservaModel } from "./models/ReservaModel";
+import loadSampleData from "./utils/loadSampleData";
+import { ReservaView } from "./views/ReservaView";
+
+await loadSampleData();
+const reservaModel = new ReservaModel();
+const reservaController = new ReservaController(reservaModel);
+const reservaView = new ReservaView(reservaController);
+
+if (window.location.pathname === "/index.html") {
+  reservaView.listar();
+}
+
+if (window.location.pathname === "/cadastro.html") {
+  reservaView.registrar();
+}
